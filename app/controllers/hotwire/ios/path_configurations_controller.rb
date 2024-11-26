9ab26316ac62b37e6b9734d38e8afabae28ab7e5
Chris Oliver <excid3@gmail.com>
2024-11-26 12:16:34 -0600

    Refactor to Hotwire Native

---
+++ app/controllers/hotwire/ios/path_configurations_controller.rb
@@ -0,0 +1,39 @@
+class Hotwire::Ios::PathConfigurationsController < ApplicationController
+  def show
+    render json: {
+      settings: {
+        tabs: [
+          {
+            title: "Home",
+            path: root_path,
+            ios_system_image_name: "house"
+          },
+          {
+            title: "Posts",
+            path: posts_path,
+            ios_system_image_name: "square.and.pencil"
+          }
+        ]
+      },
+      rules: [
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            presentation: "modal"
+          }
+        },
+        {
+          patterns: [
+            "/users/sign_in"
+          ],
+          properties: {
+            flow: "authentication"
+          }
+        }
+      ]
+    }
+  end
+end
