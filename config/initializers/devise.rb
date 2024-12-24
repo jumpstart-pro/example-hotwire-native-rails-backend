d0356a33dd9faf6ffd6dd102dbf0fac9296186e1
Joe Masilotti <joe@masilotti.com>
2024-12-24 09:58:17 -0800

    Handle /unauthorized

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# Required to handle tabs that require authentication to present /unauthorized.
+class TurboFailureApp < Devise::FailureApp
+  # Compatibility for Turbo::Native::Navigation
+  class << self
+    def helper_method(*methods)
+    end
+  end
+
+  include Turbo::Native::Navigation
+
+  def http_auth?
+    hotwire_native_app? || super
+  end
+end
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -277,8 +292,9 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  # end
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
