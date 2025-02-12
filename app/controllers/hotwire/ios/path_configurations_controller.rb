f0608e200acc33f9a13aa436bca884e69753b49e
Bibek Shrestha <bibekshrestha@gmail.com>
2025-02-12 11:37:06 -0800

    Show hide tabs based on user login

--- app/controllers/hotwire/ios/path_configurations_controller.rb
+++ app/controllers/hotwire/ios/path_configurations_controller.rb
@@ -1,19 +1,24 @@
 class Hotwire::Ios::PathConfigurationsController < ApplicationController
   def show
+    tabs = []
+    if current_user
+      tabs = [
+        {
+          title: "Home",
+          path: root_path,
+          ios_system_image_name: "house"
+        },
+        {
+          title: "Posts",
+          path: posts_path,
+          ios_system_image_name: "square.and.pencil"
+        }
+      ]
+    end
+
     render json: {
       settings: {
-        tabs: [
-          {
-            title: "Home",
-            path: root_path,
-            ios_system_image_name: "house"
-          },
-          {
-            title: "Posts",
-            path: posts_path,
-            ios_system_image_name: "square.and.pencil"
-          }
-        ]
+        tabs: tabs
       },
       rules: [
         {
