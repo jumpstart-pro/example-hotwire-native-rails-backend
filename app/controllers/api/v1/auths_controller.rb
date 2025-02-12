cce0e92947442f5d615d99882c8a4725d711cd0f
Bibek Shrestha <bibekshrestha@gmail.com>
2025-02-12 11:33:12 -0800

    Skip csrv token verification on auth controller.

--- app/controllers/api/v1/auths_controller.rb
+++ app/controllers/api/v1/auths_controller.rb
@@ -1,6 +1,8 @@
 module API
   module V1
     class AuthsController < ApplicationController
+      skip_before_action :verify_authenticity_token, only: [:destroy]
+
       def destroy
         current_user.notification_tokens
           .find_by(token: params[:notification_token])&.destroy
